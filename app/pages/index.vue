9468e55d1f2114eef0e058cc214aa22872b9730f
kikimetal <reezafratzitz2@gmail.com>
2019-01-17 00:43:00 +0900

    仮レイアウト構築

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,65 +1,70 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        slack-auto-notice
-      </h1>
-      <h2 class="subtitle">
-        
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+<template lang="html">
+  <div class="page">
+    <h1>Hello! Slack.</h1>
+    <p>{{content}}</p>
+    <div id="btn">
+      button
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
+  data() {
+    return {
+      content: {
+        attachments: [
+          {
+            fallback: 'fallback Test',
+            pretext: 'attachments Test',
+            color: '#D00000',
+            fields: [
+              {
+                title: 'attachment01',
+                value: 'This is attachment',
+              },
+            ],
+          },
+          {
+            fallback: 'fallback Test',
+            pretext: 'attachments Test02',
+            color: '#00FF00',
+            fields: [
+              {
+                title: 'attachment02',
+                value: 'This is attachment02',
+              },
+            ],
+          },
+        ],
+      },
+    }
+  },
+  methods: {},
 }
 </script>
 
-<style>
-.container {
+<style lang="scss">
+.page {
+  font-family: helvetica, sans-serif;
+  background: #000;
+  color: #eee;
   min-height: 100vh;
+  text-align: center;
   display: flex;
+  flex-flow: column;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+#btn {
+  font-size: 20px;
+  color: #333;
+  cursor: pointer;
+  background: pink;
+  border-radius: 4em;
+  line-height: 4em;
+  font-weight: 900;
+  min-width: 300px;
+  margin: 2em 0;
 }
 </style>
-
