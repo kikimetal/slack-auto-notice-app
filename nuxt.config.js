00a11eed1358d851904c3ecf4703792198ee3d2b
kikimetal <reezafratzitz2@gmail.com>
2019-01-17 00:22:00 +0900

    favicon, apple-touch-icon, を変更 + iOSスタンドアロン設定追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,13 +5,38 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'slack-auto-notice',
+    title: 'hello! Slack',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, viewport-fit=cover',
+      },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/vnd.microsoft.icon',
+        href: '/favicons/favicon.ico',
+      },
+      {
+        rel: 'shortcut icon',
+        type: 'image/vnd.microsoft.icon',
+        href: '/favicons/favicon.ico',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicons/apple-touch-icon.png',
+      },
+      {
+        rel: 'apple-touch-startup-image',
+        href: '/favicons/apple-touch-icon.png',
+      },
+    ],
   },
   /*
   ** Customize the progress bar color
